6c1e3d9021cd2bc8f21c209f061b2203d4204572
Dr.Palmer <niki98tos@gmail.com>
2024-05-17 21:56:02 +0300

    update complex settings & fix some litt

--- MacOs/Pre-Install/datastores.sh
+++ MacOs/Pre-Install/datastores.sh
@@ -2,33 +2,38 @@
 
 # Install command-line tools using Homebrew.
 # Ask for the administrator password upfront.
-
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Check for Homebrew, install if we don't have it
+if ! command -v brew &> /dev/null; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Make sure we’re using the latest Homebrew.
 brew update
 
-# Install all data stores
-brew install mysql
-brew install postgresql
-brew install mongo
-brew install redis
-brew install elasticsearch
+# Function to install datastore apps
+function installDataStores() {
+  brew install mysql
+  brew install postgresql
+  brew install mongodb
+  brew install redis
+  brew install elasticsearch
+}
 
-# Install mysql workbench
-# Install Cask
-brew install caskroom/cask/brew-cask
-brew cask install --appdir="/Applications" mysqlworkbench
+# Ask if the user wants to install datastore apps
+read -p "Database applications will now be installed. Do you want to proceed? (y/n): " -n 1;
+echo "";
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    installDataStores;
+fi;
+
+# Install MySQL Workbench
+brew install --cask mysqlworkbench
 
 # Remove outdated versions from the cellar.
-brew cleanup
+brew cleanup --prune=all
